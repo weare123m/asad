613ea176274f91316b81be9446a8caca43b74f73
weare123m <54211243+weare123m@users.noreply.github.com>
2019-11-02 19:39:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,37 +100,7 @@ message.channel.send(embed);
 }
     
 });
-client.on('message' , message => {
-if (message.author.bot) return;
-if (message.content.startsWith(prefix + "contact")) {
-if (!message.channel.guild) return;
 
-
-
-let args = message.content.split(" ").slice(1).join(" ");
-
-
-client.users.get("565833332638875648").send(
-    "\n" + "**" + "? السيرفر :" + "**" +
-    "\n" + "**" + "» " + message.guild.name + "**" +
-    "\n" + "**" + " ? المرسل : " + "**" +
-    "\n" + "**" + "» " + message.author.tag + "**" +
-    "\n" + "**" + " ? الرسالة : " + "**" +
-    "\n" + "**" + args + "**")
-
-let embed = new Discord.RichEmbed()
-     .setAuthor(message.author.username, message.author.avatarURL)
-     .setDescription(':mailbox_with_mail: تم ارسال الرسالة الى صاحب البوت بنجاح')
-     .setThumbnail(message.author.avatarURL)
-     .setFooter("By : Salah")
-                                                
-
-message.channel.send(embed);
-
-
-}
-    
-});
 
 client.on('ready', () => {
     console.log(`----------------`);
